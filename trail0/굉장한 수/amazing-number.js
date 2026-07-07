a9cee4926b1a758cce5df3d0b606afff93dfafaf
hajin15 <116537960+hajin15@users.noreply.github.com>
2026-07-07 15:20:10 +0900

    [Passed] 굉장한 수 (76ms, 10MB)

---
+++ trail0/굉장한 수/amazing-number.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let N = Number(fs.readFileSync(0).toString().trim().split());
+
+if (N % 2 !== 0 && N % 3 == 0 || N % 2 == 0 && N % 5 == 0) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
